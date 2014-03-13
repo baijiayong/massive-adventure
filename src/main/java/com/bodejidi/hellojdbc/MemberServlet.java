b18e32de695ca0ef8d297459afebfa572beeef7d
baijiayong <jiayongbai@gmail.com>
2014-03-13 23:47:37 +0800

    修改了 MemberServlet 中的两处语法错误 并 新增了一个 compile 编译脚本

--- src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
+++ src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
@@ -1,13 +1,13 @@
 package com.bodejidi.hellojdbc;
 
-imoprt java.io.*;
+import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.*;
 
 public class MemberServlet extends HttpServlet
 {
   public void doGet(HttpServletRequest req,HttpServletResponse resp)
-              throws ServletException.java.io.IOException
+              throws ServletException,java.io.IOException
   {
     resp.getWriter().println("This is a simple servlet program");
   }
