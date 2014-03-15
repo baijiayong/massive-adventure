30ad88318e1a092323571306dbd57d952fb2273e
baijiayong <jiayongbai@gmail.com>
2014-03-15 23:44:10 +0800

    MemberServlet.java 中新增了 dopost 和 doget 方法 新增了 deploy 方法

--- src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
+++ src/main/java/com/bodejidi/hellojdbc/MemberServlet.java
@@ -7,8 +7,19 @@ import javax.servlet.*;
 public class MemberServlet extends HttpServlet
 {
   public void doGet(HttpServletRequest req,HttpServletResponse resp)
-              throws ServletException,java.io.IOException
-  {
+              throws ServletException.java.io.IOException;
     resp.getWriter().println("This is a simple servlet program");
   }
+
+  public void doPost(HttpServletRequest req,HttpServletResponse resp)
+              throws ServletException.java.io.IOException ;
+  {
+    resp.getWriter().println(req.getParameter("first_name");
+    
+                             + " " 
+			     req.getParameter("last_name"));
+  }
 }
+
+</web-app>
+
